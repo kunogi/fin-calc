4671853b55b5ee0b2fd335f5bbd6c3b5f71d4dbd
kunogi <kunogi@hotmail.com>
2021-05-06 10:30:50 +0800

    readme for ma

--- demo.js
+++ demo.js
@@ -1,9 +1,11 @@
-const finCalc = require('./FinCalc');
+const finCalc = require('./src/FinCalc');
 
+//演示数据：
 let test_data = [];
 for (let i = 0; i < 100; i+=5) {
   test_data.push(i);
 }
 
+//Moving Average (MA) 移动平均线：
 const ma = finCalc.calcMA(test_data, 5);
 console.log('original data:', test_data, '移动平均Moving Average(MA):', ma);
