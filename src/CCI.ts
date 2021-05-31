5454ef6c3d17770b5f498538818a061b47856315
kunogi <kunogi@hotmail.com>
2021-05-31 14:50:30 +0800

    add formula to readme

--- src/CCI.ts
+++ src/CCI.ts
@@ -12,14 +12,15 @@ interface iCCI {
 }
 
 export default function (arr_: iKData[], customeData_: iCCI['param'] = { v0: 14 }): iCCI['data'][] {
-  const { v0 } = customeData_;
   let result: iCCI['data'][] = [];
+
+  const { v0 } = customeData_;
+
   let closeArr: number[] = FinUtil.genArrByProp(arr_, 'close'),
     highArr: number[] = FinUtil.genArrByProp(arr_, 'high'),
     lowArr: number[] = FinUtil.genArrByProp(arr_, 'low'),
     typArr: number[] = FinUtil.arrOp(FinUtil.arrOp(FinUtil.arrOp(highArr, lowArr, '+'), closeArr, '+'), 3, '/'),
     cciArr: number[] = FinUtil.arrOp(FinUtil.arrOp(typArr, MA(typArr, v0), '-'), FinUtil.arrOp(FinUtil.avedev(typArr, v0), 0.015, '*'), '/');
-
 
   for (let i: number = 0, l: number = arr_.length; i < l; i++) {
     result[i] = {
