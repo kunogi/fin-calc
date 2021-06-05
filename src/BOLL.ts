55436a18f118bad63fbfefcd302577b5b97fa405
kunogi <kunogi@hotmail.com>
2021-06-05 10:35:24 +0800

    prop rename

--- src/BOLL.ts
+++ src/BOLL.ts
@@ -5,8 +5,8 @@ import { iKData } from './interface/iDatas';
 interface iBOLL {
   param: {
     prop: string,
-    v0: number,
-    v1: number
+    v0: number,//PERIOD
+    v1: number//TIMES
   },
   data: {
     boll: number,
@@ -24,14 +24,14 @@ interface iBOLL {
   UPPER=MID + TIMES*STD(CLOSE,PERIOD)
   LOWER=MID - TIMES*STD(CLOSE,PERIOD)
  */
-export default function (arr_: iKData[], customData_: iBOLL['param'] = { prop: 'close', v0: 20, v1: 2 }): iBOLL['data'][]{
+export default function (arr_: iKData[], customData_: iBOLL['param'] = { prop: 'close', v0: 20, v1: 2 }): iBOLL['data'][] {
   let result: iBOLL['data'][] = new Array(arr_.length);
 
-  const { prop, v0, v1 } = customData_;
+  const { prop, v0: PERIOD, v1: TIMES } = customData_;
 
   let arr: number[] = FinUtil.genArrByProp(arr_, prop),
-    maArr: number[] = MA(arr, v0),
-    stdArr: number[] = FinUtil.arrOp(FinUtil.std(arr, v0), v1, '*'),
+    maArr: number[] = MA(arr, PERIOD),
+    stdArr: number[] = FinUtil.arrOp(FinUtil.std(arr, PERIOD), TIMES, '*'),
     upperArr: number[] = FinUtil.arrOp(maArr, stdArr, '+'),
     lowerArr: number[] = FinUtil.arrOp(maArr, stdArr, '-');
 
