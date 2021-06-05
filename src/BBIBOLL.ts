95f19a75a1b24c27f10230d6b7727e35551bed82
kunogi <kunogi@hotmail.com>
2021-06-05 10:32:28 +0800

    bbiboll formular

--- src/BBIBOLL.ts
+++ src/BBIBOLL.ts
@@ -5,8 +5,8 @@ import MA from './MA';
 interface iBBIBOLL {
   param: {
     prop: string,
-    v0: number,
-    v1: number
+    v0: number,//N
+    v1: number//M
   },
   data: {
     bbiboll: number,
@@ -14,19 +14,41 @@ interface iBBIBOLL {
     dwn: number
   }
 }
+
+/**
+ * 
+ * @param arr_ 
+ * @param customData_ 
+ * @returns 
+ * @description
+  BBIBOLL:(MA(CLOSE,3)+MA(CLOSE,6)+MA(CLOSE,12)+MA(CLOSE,24))/4
+  UPR:BBIBOLL+M*STD(BBIBOLL,N)
+  DWN:BBIBOLL-M*STD(BBIBOLL,N)
+ */
 export default function (arr_: iKData[], customData_: iBBIBOLL['param'] = { prop: 'close', v0: 11, v1: 6 }): iBBIBOLL['data'][] {
-  const { prop, v0, v1 } = customData_;
   let result: iBBIBOLL['data'][] = [];
-  let closeArr: number[] = FinUtil.genArrByProp(arr_, prop),
-    bbiArr: number[] = FinUtil.arrOp(FinUtil.arrOp(FinUtil.arrOp(FinUtil.arrOp(MA(closeArr, 3), MA(closeArr, 6), '+'), MA(closeArr, 12), '+'), MA(closeArr, 24), '+'), 4, '/'),
-    uprArr: number[] = FinUtil.arrOp(bbiArr, FinUtil.arrOp(FinUtil.std(bbiArr, v0), v1, '*'), '+'),
-    dwnArr: number[] = FinUtil.arrOp(bbiArr, FinUtil.arrOp(FinUtil.std(bbiArr, v0), v1, '*'), '-');
-  for (let i:number = 0, l:number = arr_.length; i < l; i++) {
+
+  const { prop, v0: N, v1: M } = customData_;
+
+  let closeArr: number[] = FinUtil.genArrByProp(arr_, prop);
+
+  //BBIBOLL:(MA(CLOSE,3)+MA(CLOSE,6)+MA(CLOSE,12)+MA(CLOSE,24))/4:
+  let bbiArr: number[] = FinUtil.arrOp(FinUtil.arrOp(FinUtil.arrOp(FinUtil.arrOp(
+    MA(closeArr, 3), MA(closeArr, 6), '+'), MA(closeArr, 12), '+'), MA(closeArr, 24), '+'), 4, '/'),
+
+    //UPR:BBIBOLL+M*STD(BBIBOLL,N):
+    uprArr: number[] = FinUtil.arrOp(bbiArr, FinUtil.arrOp(FinUtil.std(bbiArr, N), M, '*'), '+'),
+
+    //DWN:BBIBOLL-M*STD(BBIBOLL,N):
+    dwnArr: number[] = FinUtil.arrOp(bbiArr, FinUtil.arrOp(FinUtil.std(bbiArr, N), M, '*'), '-');
+
+  for (let i: number = 0, l: number = arr_.length; i < l; i++) {
     result[i] = {
       bbiboll: bbiArr[i],
       upr: uprArr[i],
       dwn: dwnArr[i]
     }
   }
+
   return result;
 }
