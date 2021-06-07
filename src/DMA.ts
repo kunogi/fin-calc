6230a9e104f7cf0cf58790a01278b5b5b21d8813
kunogi <kunogi@hotmail.com>
2021-06-07 09:01:52 +0800

    param rename

--- src/DMA.ts
+++ src/DMA.ts
@@ -4,9 +4,9 @@ import MA from './MA';
 
 interface iDMA {
   param: {
-    v0: number,//short
-    v1: number,//long
-    v2: number,
+    v0: number,//SHORT
+    v1: number,//LONG
+    v2: number,//M
     prop: string
   },
   data: {
@@ -24,16 +24,16 @@ interface iDMA {
   DIF = MA(CLOSE,SHORT) - MA(CLOSE,LONG)
   DMA = MA(DIF,M)
  */
-export default function (arr_: iKData[], customeData_: iDMA['param'] = { v0: 10, v1: 50, v2: 10, prop: 'close' }) {
+export default function (arr_: iKData[], customeData_: iDMA['param'] = { prop: 'close', v0: 10, v1: 50, v2: 10 }) {
   let result: iDMA['data'][] = [];
 
-  const { v0, v1, v2, prop } = customeData_;
+  const { v0: SHORT, v1: LONG, v2: M, prop } = customeData_;
 
   let arr: number[] = FinUtil.genArrByProp(arr_, prop),
-    maArr1: number[] = MA(arr, v0),
-    maArr2: number[] = MA(arr, v1),
+    maArr1: number[] = MA(arr, SHORT),
+    maArr2: number[] = MA(arr, LONG),
     difArr: number[] = FinUtil.arrOp(maArr1, maArr2, '-'),
-    amaArr: number[] = MA(difArr, v2);
+    amaArr: number[] = MA(difArr, M);
 
   for (let i: number = 0, l: number = arr_.length; i < l; i++) {
     result[i] = {
