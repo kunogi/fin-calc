502610b2bb2e00176b8cd692111a7968e09cd2cc
kunogi <kunogi@hotmail.com>
2021-06-08 15:33:55 +0800

    kdj formular

--- src/KDJ.ts
+++ src/KDJ.ts
@@ -3,9 +3,9 @@ import FinUtil from './util/FinUtil';
 
 interface iKDJ {
   param: {
-    v0: number,
-    v1: number,
-    v2: number
+    P1: number,//P1
+    P2: number,//P2
+    P3: number//P3
   },
   data: {
     k: number,
@@ -13,16 +13,34 @@ interface iKDJ {
     j: number
   }
 }
-export default function (arr_: iKData[], customData_: iKDJ['param'] = { v0: 9, v1: 3, v2: 3 }): iKDJ['data'][] {
-  const { v0, v1, v2 } = customData_;
+
+/**
+ * 
+ * @param arr_ 
+ * @param customData_ 
+ * @returns 
+ * @description
+  RSV=(CLOSE-LLV(LOW,P1))/(HHV(HIGH,P1)-LLV(LOW,P1))*100
+  K = SMA(RSV,P2,1)
+  D = SMA(K,P3,1)
+  J = 3*K-2*D
+ */
+export default function (arr_: iKData[], customData_: iKDJ['param'] = { P1: 9, P2: 3, P3: 3 }): iKDJ['data'][] {
   let result: iKDJ['data'][] = [];
+
+  const { P1: P1, P2: P2, P3: P3 } = customData_;
+
   let closeArr: number[] = FinUtil.genArrByProp(arr_, 'close'),
     lowArr: number[] = FinUtil.genArrByProp(arr_, 'low'),
     highArr: number[] = FinUtil.genArrByProp(arr_, 'high');
 
-  let rsvArr: number[] = FinUtil.arrOp(FinUtil.arrOp(FinUtil.arrOp(closeArr, FinUtil.llv(lowArr, v0), '-'), FinUtil.arrOp(FinUtil.hhv(highArr, v0), FinUtil.llv(lowArr, v0), '-'), '/'), 100, '*'),
-    kArr: number[] = FinUtil.sma(rsvArr, v1, 1),
-    dArr: number[] = FinUtil.sma(kArr, v2, 1),
+  //RSV=(CLOSE-LLV(LOW,P1))/(HHV(HIGH,P1)-LLV(LOW,P1))*100:
+  let rsvArr: number[] = FinUtil.arrOp(FinUtil.arrOp(
+    FinUtil.arrOp(closeArr, FinUtil.llv(lowArr, P1), '-'),
+    FinUtil.arrOp(FinUtil.hhv(highArr, P1), FinUtil.llv(lowArr, P1), '-'), '/'), 100, '*');
+
+  let kArr: number[] = FinUtil.sma(rsvArr, P2, 1),
+    dArr: number[] = FinUtil.sma(kArr, P3, 1),
     jArr: number[] = FinUtil.arrOp(FinUtil.arrOp(kArr, 3, '*'), FinUtil.arrOp(dArr, 2, '*'), '-');
 
   for (let i: number = 0, l: number = arr_.length; i < l; i++) {
