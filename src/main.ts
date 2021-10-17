84dd04e67935df20f874f2f4da7cf9930581f508
kunogi <kunogi@hotmail.com>
2021-10-17 11:20:51 +0800

    get rid of comma

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
 // import axios from 'axios'
 // axios.defaults.baseURL = '/_api_';
